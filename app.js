55174c2ef826c02281e893fcbe9db1ee12f063d3
Ryan Cooper <ryancooper729@gmail.com>
2017-03-09 00:42:37 -0500

    add mobile functionality with jQuery Mobile

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// L.mapbox.accessToken = 'pk.eyJ1IjoibWFwdGFzdGlrIiwiYSI6IjNPMkREV1kifQ.2KGPFZD0QaGfvYzXYotTXQ';
-var handle = document.getElementById('handle'),
-    start = false,
-    startTop;
-
 var map = L.map('map',{
       minZoom: 16,
       maxZoom: 19
@@ -21,22 +16,14 @@ var overlay = L.tileLayer('https://api.mapbox.com/v4/lexhousingstudies.dweq2nxv/
       })
       .addTo(map);
 
+$("#opacity-control").trigger("create");
+$("#opacity-slider").on("slidestop", function(event, ui){
+  var opacityNew = event.target.valueAsNumber;
+  ChangeOpacity(opacityNew, overlay);
+})
 
-document.onmousemove = function(e) {
-    if (!start) return;
-    // Adjust control.
-    handle.style.top = Math.max(-5, Math.min(195, startTop + parseInt(e.clientY, 10) - start)) + 'px';
-    // Adjust opacity.
-    overlay.setOpacity(1 - (handle.offsetTop / 200));
-};
-
-handle.onmousedown = function(e) {
-    // Record initial positions.
-    start = parseInt(e.clientY, 10);
-    startTop = handle.offsetTop - 5;
-    return false;
-};
-
-document.onmouseup = function(e) {
-    start = null;
-};
+function ChangeOpacity(slider, overlay){
+  var opacityValue = slider/100;
+  console.log(opacityValue);
+  overlay.setOpacity(opacityValue);
+}
